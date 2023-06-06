4891fc7c62ec7c3a9c312ca4c4464e7910cc8c7b
Rohit Singh Rathore <96055471+rohit511@users.noreply.github.com>
2023-06-06 23:21:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
 const App = () => {
-  
-}
+  const [id, setId] = useState('');
+  const [photoData, setPhotoData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    const fetchPhotoData = async () => {
+      if (id === '') {
+        setPhotoData(null);
+        return;
+      }
+
+      setLoading(true);
+
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`);
+        const data = await response.json();
+        setPhotoData(data);
+      } catch (error) {
+        console.error(error);
+      }
+
+      setLoading(false);
+    };
+
+    fetchPhotoData();
+  }, [id]);
+
+  const handleInputChange = (event) => {
+    setId(event.target.value);
+    setPhotoData(null); // Reset the photoData state when input value changes
+  };
+
+  return (
+    <div>
+      <input type="number" value={id} onChange={handleInputChange} />
+      {loading && <Loader />} {/* Render the Loader component when loading is true */}
+      {photoData && !loading && <PhotoFrame url={photoData.url} title={photoData.title} />} {/* Render the PhotoFrame component when photoData is available and loading is false */}
+    </div>
+  );
+};
 
 export default App;
